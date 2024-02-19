6ba13d065bbecb7e2df42764b6d292ed8b92f4b8
burcuustael <burcuustael>
2024-02-19 10:40:19 +0300

    Add project files.

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.APP;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange 
+
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act 
+
+            var total = calculator.add(a, b);
+
+            //Assert
+
+            Assert.Equal<int>(25,total);
+        }
+    }
+}
